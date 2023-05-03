3f58f88ac125685f5149fdaf234bbd2f9568cabc
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-05-03 11:49:13 -0400

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,26 +1,28 @@
 import ConsoleRenderer;
+import mods.StringText;
 
 public class Example {
 
 	public static void main(String[] args) {
-		// Instances The Console Renderer Class
-		ConsoleRenderer cr = new ConsoleRenderer();
 		
-		// Sets Screen Size to: WIDTH = 6, HEIGHT = 3;
-		cr.Screen_Setup(6, 3);
+		// Sets up the Console Renderer Screen Size to: WIDTH = 6, HEIGHT = 3;
+		ConsoleRenderer.Screen_Setup(64, 64);
 		
 		// Makes a Smiley Face in ACII
-		cr.Coordinate(3, 1, "|");
-		cr.Coordinate(5, 1, "|");
-		cr.Coordinate(4, 2, "^");
-		cr.Coordinate(2, 3, "\\");
-		cr.Coordinate(3, 3, "_");
-		cr.Coordinate(4, 3, "_");
-		cr.Coordinate(5, 3, "_");
-		cr.Coordinate(6, 3, "/");
+		ConsoleRenderer.Coordinate(3, 1, "|");
+		ConsoleRenderer.Coordinate(5, 1, "|");
+		ConsoleRenderer.Coordinate(4, 2, "^");
+		ConsoleRenderer.Coordinate(2, 3, "\\");
+		ConsoleRenderer.Coordinate(3, 3, "_");
+		ConsoleRenderer.Coordinate(4, 3, "_");
+		ConsoleRenderer.Coordinate(5, 3, "_");
+		ConsoleRenderer.Coordinate(6, 3, "/");
+		
+		// Uses the StringText mod for the ConsoleRenderer to render a string
+		StringText.CoordinateString(0, 0, "Hello World");
 		
 		// Renders The Coordinates
-		cr.Render();
+		ConsoleRenderer.Render();
 	}
 
 }
