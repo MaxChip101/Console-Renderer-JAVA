d4b6f447690a8bdd6ca6dbfeb415b71ff4880b3a
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-05-03 09:26:53 -0400

    Create StringText.java

---
+++ Mods/StringText.java
@@ -0,0 +1,14 @@
+import ConsoleRenderer;
+
+public class StringText {
+
+	public static void CoordinateString(int X, int Y, String string) {
+		String[] list = string.split("");
+		int i = 0;
+		for(String s : list) {
+			ConsoleRenderer.Coordinate(X + i, Y, s);
+			i++;
+		}
+		
+	}
+}
