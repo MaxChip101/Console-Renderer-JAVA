b88ebd8c4e20c322a3b9ceb6872c7903b479007f
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-04 18:45:02 -0400

    optimized the for loop

--- Mods/StringText.java
+++ Mods/StringText.java
@@ -6,10 +6,8 @@ public class StringText {
 
 	public static void CoordinateString(int X, int Y, String string) {
 		String[] list = string.split("");
-		int i = 0;
-		for(String s : list) {
-			ConsoleRenderer.Coordinate(X + i, Y, s);
-			i++;
+		for(int i = 0; i < list.length; i++) {
+			ConsoleRenderer.Coordinate(X + i, Y, list[i]);
 		}
 		
 	}
